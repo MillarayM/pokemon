0b42dc729b8d5873e4f9d57f048d4fe6e9083306
MillarayM <millaray.sw@gmail.com>
2020-10-20 11:00:59 -0300

    Imagenes

--- assets/js/index.js
+++ assets/js/index.js
@@ -21,47 +21,39 @@ function consultaAPI(nombrePokemon) {
 
       $("#pokemon").html(data.name);
       $("#identificadorPokemon").html(data.id)
-      $("#pesoPokemon").html(data.weight / 100)
+      $("#pesoPokemon").html(data.weight / 10)
       $("#alturapokemon").html(data.height / 10)
       $("#tipopokemon").html(data.types[0].type.name)
-
-      // variable tipo Pokemon
-      var tipoPokemon = data.types[0].type.name
-
-
 
 
       // variable imagenes
       var imagensvg = data.sprites.other.dream_world.front_default
       var imagenpng = data.sprites.other["official-artwork"].front_default
-      var imagenbaila = (`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${data.id}.gif`)
-
-      // data.sprites.versions["generation-v[1]"].animated.front_default
+      var imagenbaila = data.sprites.versions['generation-v']['black-white'].animated.front_default
+      
 
 
       // imagen svg
       if (imagensvg !== null) {
         $("#imagenFrente").attr("src", data.sprites.other.dream_world.front_default);
 
-      } else if (imagenpng !== null) { $("#imagenFrente").attr("src", data.sprites.other["official-artwork"].front_default) }
+      } else if (imagenpng !== null) {
+         $("#imagenFrente").attr("src", data.sprites.other["official-artwork"].front_default) }
       else {
-        $("#imagenFrente").attr("src", `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.id}.png`)
+        $("#imagenFrente").attr("src", data.sprites.front_default)
       }
 
       // imagen oficial
       if (imagenpng !== null) {
         $("#imagenOficial").attr("src", data.sprites.other["official-artwork"].front_default)
       }
-      else { $("#imagenOficial").attr("src", `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.id}.png`) }
+      else { $("#imagenOficial").attr("src", data.sprites.front_default) }
 
       // imagen baila
-      if (imagenbaila !== "") {
-        $("#imagenBaila").attr("src", `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${data.id}.gif`);
+   if (imagenbaila !== null) {
+     $("#imagenBaila").attr("src", data.sprites.versions['generation-v']['black-white'].animated.front_default);
 
-      } else { $("#imagenBaila").attr("src", `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.id}.png`) }
-
-
-
+     } else { $("#imagenBaila").attr("src", data.sprites.front_default) }
 
       let estadisticaPokemon = puntosVida = data.stats[0].base_stat; puntosAtaque = data.stats[1].base_stat; puntosDefensa = data.stats[2].base_stat; ataqueEspecial = data.stats[3].base_stat; defensaEspecial = data.stats[4].base_stat; velocidad = data.stats[5].base_stat;
       canvas(estadisticaPokemon)
@@ -110,148 +102,3 @@ function canvas(estadisticaPokemon) {
 $("#paraToggle").click(function () {
   $("#chartContainer").slideToggle();
 });
-
-/* $("#imagenFrente").click(function(){
-   $("div").animate({left: '250px'});
- }); */
-
-// tipos para clases para iconos
-var tipoPokemonArray = [
-
-  {
-    nombreTipo: "bug",
-    nombreTipoEsp: "bicho",
-    colorTipo: "#A8B820",
-    imagenTipo: "tiposicono/Bug.png"
-  },
-
-  {
-    nombreTipo: "dark",
-    nombreTipoEsp: "siniestro",
-    colorTipo: "#7A5848",
-    imagenTipo: "tiposicono/Dark.png"
-  },
-
-  {
-    nombreTipo: "dragon",
-    nombreTipoEsp: "dragón",
-    colorTipo: "#7860E0",
-    imagenTipo: "tiposicono/Dragon.png"
-  },
-
-  {
-    nombreTipo: "electric",
-    nombreTipoEsp: "eléctrico",
-    colorTipo: "#F8D030",
-    imagenTipo: "tiposicono/Electric.png"
-  },
-
-  {
-    nombreTipo: "fairy",
-    nombreTipoEsp: "hada",
-    colorTipo: "#E79FE7",
-    imagenTipo: "tiposicono/Fairy.png"
-  },
-
-  {
-    nombreTipo: "fighting",
-    nombreTipoEsp: "lucha",
-    colorTipo: "#A05038",
-    imagenTipo: "tiposicono/Fighting.png"
-  },
-
-
-  {
-    nombreTipo: "fire",
-    nombreTipoEsp: "fuego",
-    colorTipo: "#F05030",
-    imagenTipo: "tiposicono/Firepng"
-  },
-
-  {
-    nombreTipo: "flying",
-    nombreTipoEsp: "volador",
-    colorTipo: "#98A8F0",
-    imagenTipo: "tiposicono/Flying.png"
-  },
-
-  {
-    nombreTipo: "ghost",
-    nombreTipoEsp: "fantasma",
-    colorTipo: "#6060B0",
-    imagenTipo: "tiposicono/Ghost.png"
-  },
-
-  {
-    nombreTipo: "grass",
-    nombreTipoEsp: "planta",
-    colorTipo: "#78C850",
-    imagenTipo: "tiposicono/Grass.png"
-  },
-
-  {
-    nombreTipo: "ground",
-    nombreTipoEsp: "tierra",
-    colorTipo: "#E9D6A4",
-    imagenTipo: "tiposicono/Ground.png"
-  },
-  {
-    nombreTipo: "ice",
-    nombreTipoEsp: "hielo",
-    colorTipo: "#58C8E0",
-    imagenTipo: "tiposicono/Ice.png"
-  },
-
-  {
-    nombreTipo: "normal",
-    nombreTipoEsp: "normal",
-    colorTipo: "#A8A090",
-    imagenTipo: "tiposicono/Normal.png"
-  },
-
-  {
-    nombreTipo: "poison",
-    nombreTipoEsp: "veneno",
-    colorTipo: "#B058A0",
-    imagenTipo: "tiposicono/Poison.png"
-  },
-
-  {
-    nombreTipo: "psychic",
-    nombreTipoEsp: "psíquico",
-    colorTipo: "#F870A0",
-    imagenTipo: "tiposicono/Psychic.png"
-  },
-
-  {
-    nombreTipo: "rock",
-    nombreTipoEsp: "roca",
-    colorTipo: "#B8A058",
-    imagenTipo: "tiposicono/Rock.png"
-  },
-
-  {
-    nombreTipo: "steel",
-    nombreTipoEsp: "acero",
-    colorTipo: "#A8A8C0",
-    imagenTipo: "tiposicono/Steel.png"
-  },
-
-
-  {
-    nombreTipo: "water",
-    nombreTipoEsp: "agua",
-    colorTipo: "#3899F8",
-    imagenTipo: "tiposicono/Water.png"
-  },
-];
-
-
-
-// cambiar colore segun tipo de pokemon
-function tipoPokemonModal(){
-
-
-  $("#colorFondoTipoPokemon").ccs("background-color", "colorTipo")
-  $("#imagenTipoPokemon").attr("src", tipoPokemonArray.imagenTipo)
-}
